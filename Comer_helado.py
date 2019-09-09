b49321034eecfb53ce23836920182bbecaf96cd6
juandp333 <juandp333@gmail.com>
2019-09-09 10:47:54 -0500

    comer_helado_completo

--- Comer_helado.py
+++ Comer_helado.py
@@ -1,9 +1,25 @@
-apetece_helado = input("¿Te apetece comer un helado?: (SI / NO)" ).upper()
-tienes_dinero = input("¿Tienes dinero para un helado?: (SI / NO)").upper()
-esta_el_senor_de_los_helados = input("¿Esta el Señor de los helados?: (SI / NO)").upper()
-esta_tia = input("¿Estas con tu Tia?: (SI / NO)").upper()
 
-if apetece_helado == "SI" and (tienes_dinero == "SI" or esta_tia == "SI") and esta_el_senor_de_los_helados == "SI":
+apetece_helado_input = input("¿Te apetece comer un helado?: (SI / NO)").upper()
+
+if apetece_helado_input == "SI":
+   apetece_helado = True
+elif apetece_helado_input == "NO":
+    apetece_helado = False
+else:
+    print("Te he dicho que me digas SI o NO, no se que has dicho pero cuento como que no")
+    apetece_helado_input == False
+
+tienes_dinero_input = input("¿Tienes dinero para un helado?: (SI / NO)").upper()
+esta_el_senor_de_los_helados_input = input("¿Esta el Señor de los helados?: (SI / NO)").upper()
+esta_tia_input = input("¿Estas con tu Tia?: (SI / NO)").upper()
+
+apetece_helado = apetece_helado_input == "SI"
+tienes_dinero = tienes_dinero_input == "SI"
+esta_tu_tia = esta_tia_input == "SI"
+puedes_permitirtelo = tienes_dinero or esta_tu_tia
+esta_el_senor_de_los_helados = esta_el_senor_de_los_helados_input == "SI"
+
+if apetece_helado and puedes_permitirtelo and esta_el_senor_de_los_helados:
     print("Pues comer un helado")
 else:
     print("Pues no puedes comer helado")
